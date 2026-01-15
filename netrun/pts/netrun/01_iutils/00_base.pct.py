75a0371b3c4b027f71fed70c29e79848063b05e3
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2026-01-15 16:08:24 +0000

    feat(netrun._iutils): Changed structure of iutils and also added hashing functions

---
+++ netrun/pts/netrun/01_iutils/00_base.pct.py
@@ -0,0 +1,32 @@
+# ---
+# jupyter:
+#   kernelspec:
+#     display_name: .venv
+#     language: python
+#     name: python3
+# ---
+
+# %%
+#|default_exp _iutils._base
+
+# %%
+#|hide
+from nblite import nbl_export, show_doc
+
+nbl_export()
+import netrun._iutils._base as this_module
+
+# %%
+#|hide
+show_doc(this_module.patch_to)
+
+# %%
+#|export
+def patch_to(cls):
+    """Patch a class to add a method."""
+
+    def decorator(func):
+        setattr(cls, func.__name__, func)
+        return func
+
+    return decorator
