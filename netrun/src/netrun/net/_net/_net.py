0b7c398ce644be0d4a275ea9d6e5a1feb8e89cc9
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2026-02-06 14:06:11 +0000

    feat: Net.drain_queue -> Net.flush_output_queue

--- netrun/src/netrun/net/_net/_net.py
+++ netrun/src/netrun/net/_net/_net.py
@@ -347,7 +347,7 @@ class Net:
         except asyncio.QueueEmpty:
             return None
 
-    def drain_queue(
+    def flush_output_queue(
         self,
         queue_name: str | None = None,
         *,
@@ -379,7 +379,7 @@ class Net:
                 break
         return packets
 
-    def get_all_outputs(self) -> dict[str, list[OutputPacket]]:
+    def flush_all_output_queues(self) -> dict[str, list[OutputPacket]]:
         """Get all currently available packets from all queues.
 
         Non-blocking - drains all queues and returns their contents.
@@ -389,7 +389,7 @@ class Net:
         """
         result = {}
         for queue_name in list(self._output_queues.keys()):
-            packets = self.drain_queue(queue_name)
+            packets = self.flush_output_queue(queue_name)
             if packets:  # Only include non-empty queues
                 result[queue_name] = packets
         return result
